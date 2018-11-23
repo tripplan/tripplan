16541eedf2b7ad2d14f7d385136d0e2459433e28
linorabolini <linorabolini@gmail.com>
2018-11-23 02:19:43 +0100

    configured theme

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import { hot } from "react-hot-loader"
 import Routes from "containers/Routes"
 import AuthProvider from "providers/Auth"
 import CssBaseline from "@material-ui/core/CssBaseline"
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import blue from "@material-ui/core/colors/blue"
+import pink from "@material-ui/core/colors/pink"
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+        secondary: pink
+    },
+    typography: {
+        useNextVariants: true
+    }
+})
 
 class App extends React.Component {
     render = () => (
-        <AuthProvider>
-            <CssBaseline />
-            <Routes />
-        </AuthProvider>
+        <MuiThemeProvider theme={theme}>
+            <AuthProvider>
+                <CssBaseline />
+                <Routes />
+            </AuthProvider>
+        </MuiThemeProvider>
     )
 }
 
--- src/containers/NavBar/index.js
+++ src/containers/NavBar/index.js
@@ -1,16 +1,13 @@
 import Login from "containers/Login"
+import { Button } from "ui"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
 
 export default () => (
-    <div>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/trips">Trips</Link>
-            </li>
-        </ul>
-        <Login />
-        <hr />
-    </div>
+    <AppBar position="static">
+        <Toolbar variant="dense">
+            <Button component={Link} to="/" children="Home" style={{ flexGrow: 1 }} />
+            <Login />
+        </Toolbar>
+    </AppBar>
 )
